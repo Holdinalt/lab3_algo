c228cc40243a438b5d49a324740b5d314c6ab5ab
Holdinalt <zbsholdest@mail.ru>
2021-03-01 19:49:17 +0300

    Optimise

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 #include <map>
 #include <vector>
-#include <stack>
 
-class my_stack : public std::stack<std::map <std::string, int>> {
-public:
-    using std::stack<std::map <std::string, int>>::c; // expose the container
-};
-
+std::map <std::string, int> makeCacheMap();
 void startBlock();
 int getValFromMap(std::string);
 void blockStart();
@@ -15,16 +10,16 @@ void blockEnd();
 
 int cacheTime = 50;
 
-my_stack mapVector;
+std::vector<std::map <std::string, int>> mapVector;
 
-std::map <std::string, int> cacheMap;
+
 
 int main() {
 
 
     std::string input;
 
-    mapVector.push(std::map <std::string, int>()); //addMap
+    mapVector.emplace_back(); //addMap
 
     while (std::cin >> input) {
 
@@ -37,14 +32,23 @@ int main() {
 
         int separator = input.find('=');
 
-        try{
+        bool variable = true;
+
+        for (char elem : input.substr(separator + 1)){
+            unsigned int intElem = (int)elem;
+            if ((intElem >= 48 && intElem <=57) || intElem == 45){
+                variable = false;
+            }
+        }
+
+        if(variable){
+            int argValue = mapVector[0][input.substr(separator + 1)]; //variable
+            std::cout << argValue << "\n";
+            mapVector[0][input.substr(0, separator)] = argValue;
+        } else{
             int number = std::stoi(input.substr(separator + 1)); //number
             //std::cout << input.substr(0, separator) << " = " << number << "\n";
-            mapVector.top()[input.substr(0, separator)] = number;
-        } catch (...){
-            int argValue = mapVector.top()[input.substr(separator + 1)]; //variable
-            std::cout << argValue << "\n";
-            mapVector.top()[input.substr(0, separator)] = argValue;
+            mapVector[0][input.substr(0, separator)] = number;
         }
     }
 
@@ -67,17 +71,24 @@ void startBlock(){
 
         int separator = input.find('=');
 
-        try{
-            int number = std::stoi(input.substr(separator + 1)); //number
-            //std::cout << input.substr(0, separator) << " = " << number << "\n";
-            mapVector.top()[input.substr(0, separator)] = number;
-            cacheMap[input.substr(0, separator)] = number;
-        } catch (...){
+        bool variable = true;
+
+        for (char elem : input.substr(separator + 1)){
+            unsigned int intElem = (int)elem;
+            if ((intElem >= 48 && intElem <=57) || intElem == 45){
+                variable = false;
+            }
+        }
+
+        if(variable){
             int argValue = getValFromMap(input.substr(separator + 1)); //variable
 
             std::cout << argValue << "\n";
-            mapVector.top()[input.substr(0, separator)] = argValue;
-            cacheMap[input.substr(0, separator)] = argValue;
+            mapVector.back()[input.substr(0, separator)] = argValue;
+        } else{
+            int number = std::stoi(input.substr(separator + 1)); //number
+            //std::cout << input.substr(0, separator) << " = " << number << "\n";
+            mapVector.back()[input.substr(0, separator)] = number;
         }
         std::cin >> input;
     }
@@ -86,11 +97,11 @@ void startBlock(){
 }
 
 int getValFromMap(const std::string str){
+    int i = (int)mapVector.size() - 1;
 
-    unsigned int i = mapVector.size();
-    while (i > 0){
-        if(mapVector.c[i-1].count(str) == 1){
-            return mapVector.c[i-1][str];
+    while (i > -1){
+        if(mapVector[i].count(str) == 1){
+            return mapVector[i][str];
         }
         i--;
     }
@@ -99,14 +110,26 @@ int getValFromMap(const std::string str){
 
 void blockStart(){
     if(mapVector.size() % cacheTime == 0){
-        mapVector.push(cacheMap);
+        mapVector.push_back(makeCacheMap());
     }
-    mapVector.push(std::map <std::string, int>()); //addMap
+    mapVector.emplace_back();
 }
 
 void blockEnd(){
-    mapVector.pop();
+    mapVector.pop_back();
     if(mapVector.size() % cacheTime == 0){
-        mapVector.pop();
+        mapVector.pop_back();
     }
+}
+
+std::map <std::string, int> makeCacheMap(){
+
+    std::map <std::string, int> cacheMap;
+
+    for (std::map <std::string, int> map : mapVector){
+        for(auto & it : map) {
+            cacheMap[it.first] = it.second;
+        }
+    }
+    return cacheMap;
 }
